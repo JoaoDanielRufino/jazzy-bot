7971f4ae58ba588e729830ad929ae9ce2045e5f2
JoaoDanielRufino <joaodaniel0405@gmail.com>
2021-11-07 13:36:58 -0300

    v2 poc
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,106 @@
-import { Client } from 'discord.js';
+import {
+  createAudioPlayer,
+  joinVoiceChannel,
+  createAudioResource,
+} from '@discordjs/voice';
+import { Client, Intents } from 'discord.js';
 import * as dotenv from 'dotenv';
-import SarveBot from './SarveBot';
-
-const client = new Client();
+import ytdl from 'ytdl-core';
 
 dotenv.config();
 
-client.login(process.env.BOT_TOKEN)
-  .then(() => {
-    new SarveBot(client);
-  })
-  .catch(err => console.log(err));
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+});
+
+client.on('ready', () => {
+  console.log('Bot ready');
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.content == 'sarve play') {
+    if (!message.member?.voice.channel) {
+      message.reply('You need to be in a voice channel to play song');
+      return;
+    }
+
+    const voiceChannel = message.member!.voice.channel;
+    const connection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: voiceChannel.guild.id,
+      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+    });
+
+    connection.on('error', (err) => console.log(err));
+
+    const audioPlayer = createAudioPlayer();
+
+    connection.subscribe(audioPlayer);
+
+    audioPlayer.play(
+      createAudioResource(
+        ytdl('https://www.youtube.com/watch?v=rPLLXQDNoFI', {
+          filter: 'audioonly',
+          quality: 'highestaudio',
+        })
+      )
+    );
+
+    audioPlayer.on('error', (err) => console.log(err));
+  }
+});
+
+// function createAudioResource() {
+//   return new Promise((resolve, reject) => {
+//     const process = ytdl(
+//       'https://www.youtube.com/watch?v=rPLLXQDNoFI',
+//       {
+//         o: '-',
+//         q: '',
+//         f: 'bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio',
+//         r: '100K',
+//       },
+//       { stdio: ['ignore', 'pipe', 'ignore'] }
+//     );
+//
+//     if (!process.stdout) {
+//       reject(new Error('No stdout'));
+//
+//       return;
+//     }
+//
+//     const stream = process.stdout;
+//
+//     const onError = (error: Error) => {
+//       if (!process.killed) process.kill();
+//
+//       stream.resume();
+//
+//       reject(error);
+//     };
+//
+//     process
+//
+//       .once('spawn', () => {
+//         demuxProbe(stream)
+//           .then((probe: { stream: any; type: any }) =>
+//             resolve(
+//               createAudioResource(probe.stream, {
+//                 metadata: this,
+//                 inputType: probe.type,
+//               })
+//             )
+//           )
+//
+//           .catch(onError);
+//       })
+//
+//       .catch(onError);
+//   });
+// }
+
+client.login(process.env.BOT_TOKEN);
