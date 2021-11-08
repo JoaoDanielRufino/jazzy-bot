9af93a28af5a80b540eb78ff01706b248f443f59
JoaoDanielRufino <joaodaniel0405@gmail.com>
2021-11-08 01:10:56 -0300

    utils
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- src/MusicPlayer/index.ts
+++ src/MusicPlayer/index.ts
@@ -6,6 +6,7 @@ import {
 } from '@discordjs/voice';
 import ytdl from 'ytdl-core';
 import sambasPlaylist from './playlists/sambas.json';
+import { randomIndex } from './utils';
 
 export class MusicPlayer {
   private connection?: VoiceConnection;
@@ -17,10 +18,9 @@ export class MusicPlayer {
   }
 
   public playSamba() {
+    const sambaIndex = randomIndex(this.sambas);
     this.audioPlayer.play(
-      createAudioResource(
-        ytdl(this.sambas[0].url, { filter: 'audioonly', quality: 'highestaudio' })
-      )
+      createAudioResource(ytdl(this.sambas[sambaIndex].url, { filter: 'audioonly', quality: 'highestaudio' }))
     );
   }
 
