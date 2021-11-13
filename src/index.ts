93c8fe483c39badd1354ac0b97ad8083dca548c1
JoaoDanielRufino <joaodaniel0405@gmail.com>
2021-11-13 17:56:20 -0300

    get token via ssm aws
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Client, Intents } from 'discord.js';
+import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm';
 import * as dotenv from 'dotenv';
 import SarveBot from './SarveBot';
 
@@ -8,9 +9,27 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
 });
 
-client
-  .login(process.env.BOT_TOKEN)
-  .then(() => {
+async function getSSMToken() {
+  const ssmClient = new SSMClient({ region: 'us-east-1' });
+
+  const data = await ssmClient.send(
+    new GetParameterCommand({
+      Name: 'sarve-bot-parameter-store-token',
+      WithDecryption: true,
+    })
+  );
+
+  return data.Parameter?.Value;
+}
+
+async function main() {
+  try {
+    const discordToken = process.env.BOT_TOKEN || (await getSSMToken());
+    client.login(discordToken);
     new SarveBot(client);
-  })
-  .catch((err) => console.log(err));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
